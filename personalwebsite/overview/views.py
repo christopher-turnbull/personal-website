9036cc72198b31c2e33a6a2bceb4cae346b6050a
christopher-turnbull <cturnbull27995@gmail.com>
2020-12-26 10:48:51 +0300

    another commit

---
+++ personalwebsite/overview/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the overview index.")
+
+def hello_world(request):
+    return render(request, 'index.html', {})
