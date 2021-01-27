85135ff499131e533091f96d69468f6df988442f
christopher-turnbull <cturnbull27995@gmail.com>
2021-01-27 10:35:28 +0300

    Hid secret key

--- personalwebsite/personalwebsite/settings.py
+++ personalwebsite/personalwebsite/settings.py
@@ -14,6 +14,9 @@ import os
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yj-#3&c^tyf74q899%5%#ggw(f6!=)hm-^mrn_1@=tmw*@apdt'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
