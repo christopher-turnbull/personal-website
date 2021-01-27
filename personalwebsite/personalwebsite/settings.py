5ac7e269467f4d08a45fa98e0005ed037eb97c52
christopher-turnbull <cturnbull27995@gmail.com>
2021-01-27 08:23:18 +0000

    WIP

--- personalwebsite/personalwebsite/settings.py
+++ personalwebsite/personalwebsite/settings.py
@@ -27,8 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.chris-turnbull.com','webapp-1006793.pythonanywhere.com','127.0.0.1']
 
